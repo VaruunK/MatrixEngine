fab806b2d590af2e3d1a6bce6472fae7b5256468
Varuun K <113863648+VaruunK@users.noreply.github.com>
2026-06-19 22:22:28 -0400

    Delete build directory

--- build/vendored/SDL_shadercross/external/SPIRV-Cross/gitversion.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2021 The Khronos Group Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-#ifndef SPIRV_CROSS_GIT_VERSION_H_
-#define SPIRV_CROSS_GIT_VERSION_H_
-
-#define SPIRV_CROSS_GIT_REVISION "Git commit: vulkan-sdk-1.4.341.0-81-g68413a97 Timestamp: 2026-06-19T20:35:56"
-
-#endif
