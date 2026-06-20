ba7525e9259fc8a94069ca65f64bad8f283b82f7
Varuun K <113863648+VaruunK@users.noreply.github.com>
2026-06-19 22:17:32 -0400

    Delete build directory

--- build/vendored/SDL_shadercross/external/DirectXShaderCompiler/include/dxc/Test/TestConfig.h
+++
@@ -1,2 +0,0 @@
-#define DEFAULT_TEST_DIR L"C:/Users/varuu/development/Engine/vendored/SDL_shadercross/external/DirectXShaderCompiler/tools/clang/test/HLSL/"
-#define DEFAULT_EXEC_TEST_DIR L"C:/Users/varuu/development/Engine/vendored/SDL_shadercross/external/DirectXShaderCompiler/tools/clang/unittests/HLSLExec/"
